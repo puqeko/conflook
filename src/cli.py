5f04bb9e6def3abf39f458c555b2600df21227b9
puqeko <puqeko@gmail.com>
2021-10-17 15:08:15 +1300

    Make pre-commit compliant

--- src/cli.py
+++ src/cli.py
@@ -1,27 +1,40 @@
+"""
+.
+"""
+
+
 import click
 
 
 @click.group()
 @click.version_option(package_name="conflook")
 def cli():
-    pass
+    """
+    .
+    """
 
 
 @cli.command(help="Show summarised structure or value at keypath.")
 @click.option("--raw", "is_raw", is_flag=True, help="Show full value.")
 @click.argument("file", type=click.File("rb"))
 @click.argument("keypath")
+# pylint: disable=unused-argument
 def get(file, keypath, is_raw):
-    click.echo("get")
+    """
+    .
+    """
 
 
-@cli.command(help="Set the value at keypath.")
+@cli.command(name="set", help="Set the value at keypath.")
 @click.option("--use-editor", is_flag=True)
 @click.argument("file", type=click.File("rb+"))
 @click.argument("keypath")
 @click.argument("value", nargs=-1)
-def set(file, keypath, value, is_raw, use_editor):
-    click.echo("set")
+# pylint: disable=unused-argument
+def set_(file, keypath, value, is_raw, use_editor):
+    """
+    .
+    """
 
 
 if __name__ == "__main__":
